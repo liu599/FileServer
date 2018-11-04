4d986ce4f4c21ec8765c2589f42a7eb0565d1c32
Tokei <970228409@qq.com>
2018-11-05 01:35:36 +0800

    update

--- src/nekohandfileserver/main.go
+++ src/nekohandfileserver/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"nekohandfileserver/controller"
 	"os"
@@ -10,6 +9,7 @@ import (
 	"nekohandfileserver/middleware/data"
 	"nekohandfileserver/middleware/func"
 	"net/http"
+	"github.com/gin-contrib/cors"
 )
 
 
@@ -37,20 +37,21 @@ func main() {
 
 	_func.AssignDatabaseFromList([]string{"nekohand"})
 	//
-	r := gin.New()
+	r := gin.Default()
 	//
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	sysFilePath := os.Getenv("SERVER_FILE_PATH")
-	r.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token", "User", "Authorization", "X-Requested-With"},
-		ExposeHeaders:    []string{"Content-Length", "X-Real-Ip"},
-		AllowCredentials: true,
-		AllowAllOrigins:  false,
-		AllowOriginFunc:  func(origin string) bool { return true },
-		MaxAge:           86400,
-	}))
+	//r.Use(cors.New(cors.Config{
+	//	AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
+	//	AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token", "User", "Authorization", "X-Requested-With"},
+	//	ExposeHeaders:    []string{"Content-Length", "X-Real-Ip"},
+	//	AllowCredentials: true,
+	//	AllowAllOrigins:  false,
+	//	AllowOriginFunc:  func(origin string) bool { return true },
+	//	MaxAge:           86400,
+	//}))
+	r.Use(cors.Default())
 	r.MaxMultipartMemory = 8 << 20 // 8 MiB
 	r.Static("/files", sysFilePath)
 	r.StaticFS("/nhfiles", http.Dir(sysFilePath))
